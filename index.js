79a2fae12dbd675867cb9870b927eee97f14f775
Vishvesh-Shrikant <vishveshshri@gmail.com>
2025-02-05 19:15:27 +0530

    dcvsfvf

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express= require("express")
 const {config}= require("dotenv")
 const mailroutes=require("./Routes/mailRoutes")
+const cors= require('cors')
 config()
 const app=express()
+
+app.use(
+    cors({
+      credentials: true,
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
 app.use(express.json())
 app.use(mailroutes)
 
